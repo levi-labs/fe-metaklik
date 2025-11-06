74df8f0401beada24e92b3646161df585dd5a762
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-11-06 08:15:21 +0700

    fix bug

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,13 +2,16 @@
 
 import SignInForm from '@/components/signin/SignInForm';
 import SignInCarousel from '@/components/signin/SignInCarousel';
+import { Suspense } from 'react';
 
 const SignInPage = () => {
   return (
-    <div className="min-h-screen">
-      <div className="grid grid-cols-1 lg:grid-cols-2 overflow-hidden min-h-screen w-full">
+    <div className='min-h-screen'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 overflow-hidden min-h-screen w-full'>
         {/* Left Column: Login Form */}
-        <SignInForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignInForm />
+        </Suspense>
 
         {/* Right Column: Onboarding Content */}
         <SignInCarousel />
