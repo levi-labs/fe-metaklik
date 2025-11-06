ba0c36e270cc5143a577513a286f9e82b4fb9fca
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-11-06 07:27:41 +0700

    fix: link page params v4

--- src/app/(customer)/dashboard/links/[id]/page.tsx
+++ src/app/(customer)/dashboard/links/[id]/page.tsx
@@ -1,8 +1,8 @@
-export default async function DetailPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
+'use client';
+import { useParams } from 'next/navigation';
+import React from 'react';
+
+export default function DetailPage() {
+  const { id } = useParams();
   return <div>DetailPage with id: {id}</div>;
 }
